4343c27595eef1ec1e37592985d46a004e067042
bluemario8 <bluemario812@gmail.com>
2024-10-21 10:43:11 -0400

    Started on the sort function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function addGrocery(data) {
+    console.log(data)
     let parent = data.parentNode;
     let textInputValue = parent.getElementsByTagName("input")[0].value;
     let list = document.getElementsByClassName("list-items")[0].children[0];
     let elem = document.createElement("li");
     elem.className = "list-item"; 
-    elem.innerHTML = `<span>${textInputValue}</span>`
+    elem.innerHTML = `<button onclick="strike()">${textInputValue}</button>`
     list.appendChild(elem);
+}
+
+function strike() {
+
+}
+
+function sort(data) {
+    console.log(data.value);
 }
