3a79132ea081eb54ce6ef19bf3678957f7683f1d
Jose <jjmaximo13@gmail.com>
2024-10-21 16:12:22 -0400

    I made the drop down selection

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const strikethroughButtons = document.getElementsByClassName('strikeButton');
 Array.from(strikethroughButtons).forEach((button) => {
     button.onclick = () => {
         if (button.style.textDecoration === 'line-through') {
-            button.style.textDecoration = ''; 
+            button.style.textDecoration = ''
           } else {
-            button.style.textDecoration = 'line-through'; 
+            button.style.textDecoration = 'line-through'
           }
     }
 })
-
- 
+function filterButtons() {
+    const filterValue = document.getElementById('Dropdown').value
+    const buttons = document.getElementsByClassName('strikeButton')
+    Array.from(buttons).forEach((button) => {
+        switch (filterValue) {
+            case 'all':
+                button.style.display = 'inline-block'
+                break
+            case 'struck':
+                button.style.display = button.style.textDecoration === 'line-through' ? 'inline-block' : 'none'
+                break
+            case 'notstruck':
+                button.style.display = button.style.textDecoration === '' ? 'inline-block' : 'none';
+                break
+        }
+    })
+}
