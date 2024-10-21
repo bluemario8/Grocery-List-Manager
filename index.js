9b45cb23b599ae127d441fdff39198da453545d9
Jose <jjmaximo13@gmail.com>
2024-10-21 11:23:14 -0400

    removed the addeventlistner and made it to a onclick

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 
-const strikethroughButtons = document.getElementsByClassName('strikethroughButton');
+const strikethroughButtons = document.getElementsByClassName('strikeButton');
 
 Array.from(strikethroughButtons).forEach((button) => {
-  button.addEventListener('click', () => {
-
-    if (button.style.textDecoration === 'line-through') {
-      button.style.textDecoration = ''; 
-    } else {
-      button.style.textDecoration = 'line-through'; 
+    button.onclick = () => {
+        if (button.style.textDecoration === 'line-through') {
+            button.style.textDecoration = ''; 
+          } else {
+            button.style.textDecoration = 'line-through'; 
+          }
     }
-  });
-});
+})
+
  
